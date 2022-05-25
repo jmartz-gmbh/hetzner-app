2ae3bc476dd6b91e0bcce141d24b7a2f0d94f2ca
Jonathan Martz <git@jmartz.de>
2022-05-26 01:14:26 +0200

    add hetzner color

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     manifest: {
       name: 'PWA Router',
       short_name: 'PWA Router',
-      theme_color: '#ffffff',
+      theme_color: '#d50c2d',
       start_url: '/',
       icons: [{ "src": "/android-chrome-192x192.png", "sizes": "192x192", "type": "image/png" }, { "src": "/android-chrome-512x512.png", "sizes": "512x512", "type": "image/png" }]
     },
